80fafef8359c33e1310a357fdc19d866e5608dfa
Vanessa Palacios Sharma <vanelunapalacios@live.com>
2016-09-09 18:13:42 -0700

    Update Rectangle.java

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -1,25 +1,51 @@
-public class Rectangle {
-  private int mLength;
-  private int mWidth;
-  private boolean squareness;
+public class Event {
+
+  private int mGuests;
+  private int mFood;
+  private int mDrinks;
+  private int mEntertainment;
+  private int mFoodCost;
+  private int mDrinksCost;
+  private int mTotalEventCost;
 
 
-  public Rectangle(int length, int width) {
-    mLength = length;
-    mWidth = width;
+public Event( int guests, int food, int drinks, int entertainment){
+
+  mGuests = guests;
+  mFood = food;
+  mDrinks = drinks;
+  mEntertainment = entertainment;
+
+ }
+
+  public int getGuests() {
+    return mGuests;
   }
-  public int getLength() {
-    return mLength;
+
+  public int getFood() {
+    return mFood;
   }
-  public int getWidth() {
-    return mWidth;
+
+  public int getDrinks() {
+    return mDrinks;
   }
-  public boolean isSquare() {
-    if (mLength==mWidth) {
-      squareness = true;
-    } else {
-      squareness = false;
-    }
-    return squareness;
+
+  public int getEntertainment() {
+    return mEntertainment;
   }
+
+  public int getFoodCost() {
+     mFoodCost = mGuests*mFood;
+     return mFoodCost;
+}
+public int getDrinksCost() {
+  mDrinksCost = mGuests*mDrinks;
+  return mDrinksCost;
+}
+
+  public int getTotalEventCost() {
+    mTotalEventCost = (mGuests*mFood) + (mGuests*mDrinks) + mEntertainment;
+    return mTotalEventCost;
+ }
+
 }
